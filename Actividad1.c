158e00c3e93792e4c19fbf76ecd8b28753867207
VeluxRzr <Veluxbtc@gmail.com>
2025-01-28 10:00:23 +0100

    Actividad 1

---
+++ Actividad1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+    int a,b;
+    printf("introducir primer numero");
+    scanf("%d" ,&a);
+    printf("introducir segundo numero");
+    scanf("%d" ,&b);
+    printf("La suma es %d/n",a+b);
+    printf("El producto es %d/n",a*b);
+    return 0;
+}
