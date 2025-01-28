78461a8478ae4e2a2abe8e2df266d7f3c0bcc7ba
VeluxRzr <Veluxbtc@gmail.com>
2025-01-28 13:56:57 +0100

    ad

--- Actividad6.c
+++ Actividad6.c
@@ -3,13 +3,25 @@ int main(void)
 {
     int devolver,a,b,c,e ;
     /*a=monedas de 50cen, b= monedas de 20cent, c= monedas de 10 cent, d = monedas de 5 cent*/
-    printf("Indicar cantidad a devolver\n");
-    scanf("%f", &devolver);
-    printf("%d Monedas de 50\n" ,devolver);
-    a=0
-    b=0
-    c=0
-    d=0
+    printf("Indicar cantidad a devolver ");
+    scanf("%f", &euros);
+    printf("%f Monedas de 50cent\n %d Monedas de 20 cent\n %d Monedas de 10cent\n %d Monedas de 5cent " , a,b,c,d);
+    a=euros/0,50
+    if (a>0)
+    {
+        b=b/0,20
+    }
+    if (b>0)
+         
+        (c=c/0,10)
+    }
+    
+    if (c>0)
+    
+    {
+        (d=d/0,05)
+    }
+    
 
     
     return 0;
