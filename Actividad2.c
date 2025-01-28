5f9eb05b4ab2b5e5df3a74189451fc1da2b9d764
VeluxRzr <Veluxbtc@gmail.com>
2025-01-28 10:00:56 +0100

    Actividad 2

---
+++ Actividad2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+    int a; 
+    printf("Introduzca el lado del cuadrado:\n");
+    scanf("%d", &a);
+    printf("\nEl perimetro del cuadrado es: %d",a*4);
+    return 0;
+}
