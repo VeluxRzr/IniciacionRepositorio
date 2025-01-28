e5dc91dfdef9ee7ccdbe621c5bcb9e986921622f
VeluxRzr <Veluxbtc@gmail.com>
2025-01-28 11:40:12 +0100

    Actividad 2

---
+++ Actividad3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+int a,b,c,d;
+printf("Introduzca el primer numero\n");
+scanf("%d" ,&a);
+printf("Introduzca segundo numero\n");
+scanf("%d" ,&b);
+printf("Introduzca el tercer numero\n");
+scanf("%d" ,&c);
+printf("Introduzca cuarto numero\n");
+scanf("%d" ,&d);
+printf("La suma de los dos primeros es de: %d" ,a+b);
+printf("\nEl producto de los segundos es de: %d" ,c*d);
+return 0;
+}
