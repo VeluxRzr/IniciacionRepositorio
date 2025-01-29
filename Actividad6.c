c65e9a7884032801a8cf8e4c09cd5d38c73c3a6a
VeluxRzr <Veluxbtc@gmail.com>
2025-01-29 10:24:05 +0100

    actividad 6

--- Actividad6.c
+++ Actividad6.c
@@ -1,28 +1,31 @@
 #include <stdio.h>
 int main(void)
 {
-    int devolver,a,b,c,e ;
-    /*a=monedas de 50cen, b= monedas de 20cent, c= monedas de 10 cent, d = monedas de 5 cent*/
-    printf("Indicar cantidad a devolver ");
-    scanf("%f", &euros);
-    printf("%f Monedas de 50cent\n %d Monedas de 20 cent\n %d Monedas de 10cent\n %d Monedas de 5cent " , a,b,c,d);
-    a=euros/0,50
-    if (a>0)
-    {
-        b=b/0,20
-    }
-    if (b>0)
-         
-        (c=c/0,10)
-    }
+    int cincuenta,veinte,diez,cinco;
+    float cantidad, falta;
+    printf("Indicar cantidad a devolver\n");
+    scanf("%f", &cantidad);
     
-    if (c>0)
+    falta=cantidad;
     
-    {
-        (d=d/0,05)
-    }
-    
+    cincuenta=(falta/0.50);
+    printf("%d monedas de 50 cent ", cincuenta);
+    falta=falta-cincuenta*0.50;
+
+    veinte=(falta/0.20);
+    printf("%d monedas de 20 cent ", veinte);
+    falta=falta-veinte*0.20;
+
+    diez=(falta/0.10);
+    printf("%d monedas de 10 cent ", diez);
+    falta=falta-diez*0.10;
+
+    cinco=(falta/0.05);
+    printf("%d monedas de 0.5 cent ", cinco);
+
 
     
-    return 0;
+   
+     
+        return 0;
 }
