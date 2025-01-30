a0e318dc83bbf1e208bbd5118ff19dfb69edfa42
VeluxRzr <Veluxbtc@gmail.com>
2025-01-30 14:38:22 +0100

    tyj

--- Estructuras Condicionales/Actividad1.c
+++ Estructuras Condicionales/Actividad1.c
@@ -1,6 +1,19 @@
-#include <stdio>
+#include <stdio.h>
 int main(void)
 {
+    float sueldo;
+
+    printf(" Introduzca importe del sueldo \n");
+    scanf("%f" , &sueldo); 
+
+    if (sueldo>3000)
+    {
+        printf(" Debe abonar impuestos ");
+    }
+    else
+    {
+        
+    }
     
     return 0;
 }
