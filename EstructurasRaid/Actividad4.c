d5984ce50d2e755a46a64b54c159c76a1ff0e84b
VeluxRzr <Veluxbtc@gmail.com>
2025-01-31 11:56:20 +0100

    fyjug

---
+++ EstructurasRaid/Actividad4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void)
+{
+  char*cadena= "hola mundo";
+  char*pt;
+  int i:
+  pt= cadena+1;
+  
+
+  for (i=0; i<12 ;i++);
+  {
+    printf(" %c" , *pt);
+    pt+=2;
+  
+  printf("\n");
+
+    return 0;
+}
