fd3d4c73c577df412d9611220c7d5c8382ed517f
VeluxRzr <veluxbtc@gmail.com>
2025-02-05 21:40:08 +0100

    thyj

--- Arrays, Cadenas y Punteros/Actividad2.c
+++ Arrays, Cadenas y Punteros/Actividad2.c
@@ -3,18 +3,24 @@
 int main(void)
 {
     int tabla[3][2];
-    int valor
-    int tabla[i][j];
+    int i,j;
 
-
-    for (int i = 0; i < 3; i++){
-
+    for (i=0;i<3;i++)
+    {
+        for (j =0;j<2;j++)
+        {
+         printf("Introduzca los valores fila %d columna %d: " , i, j);
+         scanf("%d" , &tabla[i][j]);
+        }
+    }   
+           
+    for  (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+        printf("%d " , tabla[i][j]);
+        }
+        printf("\n");
     }
-        for (int j = 0; j < 2; j++){
-    printf("Introduzca los valores de la primera fila\n");
-    scanf("%d" , &valor);
-    tabla[i][j] = valor
-    }
-    printf(" los valores de la primera fila son %d" , valor);
     return 0;
 }
